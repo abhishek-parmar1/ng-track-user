aef2e4c7517edb19993890c9778d9500a6ffd85c
Abhishek <parmarabhishek.53@gmail.com>
2018-04-07 03:11:20 +0530

    added the functions descriptions

--- ngTrackU.js
+++ ngTrackU.js
@@ -1,4 +1,6 @@
-// TODO : add description here
+/**
+ * Imported the required utilities.
+ */
 import {
   getFirstScrollableParent,
   getOffsetTop,
@@ -6,25 +8,29 @@ import {
   visibility
 } from "./utils";
 
-// TODO : add description here
+
+/**
+ * Angular directive (using ES-6)
+ */
 const NgTrackU = () => {
+  
   return {
     restrict: "A",
     link: function (scope, element) {
+
       // get the element
       const nativeEl = element[0];
 
       // get the scrollable parent of the element
       const scrollableParent = getFirstScrollableParent(nativeEl);
 
-      //TODO: replace Scroll by set Interval and record the time for each individual id.
-
+      // set the timer to 0
       let timer = 0, 
         isPaused = false;
-
+      
+      // bind the visibly to window
       visibility();
         
-      // on scroll event on parent
       let interval = setInterval( () => {
 
         // height of parent and element
@@ -32,6 +38,7 @@ const NgTrackU = () => {
           fieldHeight = nativeEl.getBoundingClientRect().height,
 
           // dimensions of parent and element  
+          // TODO: use this property and test
           viewPortDimensions = {
             top: scrollableParent.scrollTop,
             bottom: scrollableParent.scrollTop + viewPortHeight,
@@ -44,30 +51,42 @@ const NgTrackU = () => {
             name: nativeEl.id
           };
 
+        // on tab change
         visibly.onHidden(function() {
           isPaused = true;
         });
     
+        // on current tab
         visibly.onVisible(function() {
           isPaused = false;
         });      
 
         // check if element in view
         if (isElementInView(fieldDimensions, viewPortDimensions) && !isPaused) {
+          
+          // incerment the time
           timer++;
-          // TODO: remove this console log from here
+          
+          // log the time for section
           console.log(nativeEl.id, timer);
         }
       }, 1000
     );
 
+    // on destroy of directive
     element.on('$destroy', () => {
+
+      // clear the interval to prevent memory leak
       clearInterval(interval);
       if(timer) {
-        // add mix panel call here
+        
+        // log the final time for the element
         console.log(nativeEl.id, timer);
+
+        /* Call your trackings here.*/
       }
     });
+    
     }
   }
 };
