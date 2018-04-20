a5a3aec4eebcb4a6bbcd88fadff98ea3728e1a95
Abhishek <parmarabhishek.53@gmail.com>
2018-04-20 10:01:51 +0530

    logic changed for tracker

--- src/ngTrackUser.js
+++ src/ngTrackUser.js
@@ -8,6 +8,9 @@ import {
   visibility
 } from "./utils";
 
+/* a constant for time interval limit*/
+const globalWatcherLimit = 20;
+
 
 /**
  * Angular directive (using ES-6)
@@ -24,13 +27,36 @@ const NgTrackUser = () => {
       // get the scrollable parent of the element
       const scrollableParent = getFirstScrollableParent(nativeEl);
 
-      // set the timer to 0
-      let timer = 0, 
-        isPaused = false;
-      
-      // bind the visibly to window
+      let isPaused = false;
+      // a global object which will track time of all sections
+      if(!window.hasOwnProperty('timer')) {
+        window.timer = {};
+      }
+      // a global variable which will track the overall all time spend 
+      if(!window.hasOwnProperty('globalWatcher')) {
+        window.globalWatcher = 0;
+        setInterval(() => window.globalWatcher++, 1000);
+      }
+      // add a global method which will reset the time of all sections
+      if(!window.hasOwnProperty('resetEventsTime')) {
+        window.resetEventsTime = () => {
+          Object.keys(window.timer).forEach(e => window.timer[e] = 0 );
+        }
+      }
+      // add a global method which will call the trackings for all the methods
+      if(!window.hasOwnProperty('trackingCalls')) {
+        window.trackingCalls = () => {
+          Object.keys(window.timer).forEach(e => {
+            if(window.timer[e])
+              // call here your custom data analytics method  
+              yourCustomDataAnalyticsMethod(e.toUpperCase().replace(/-/g , "_"), {timeSpend: window.timer[e]}) 
+          });
+        }
+      }
+
       visibility();
-        
+      
+      // an interval of 1 second for each section to track
       let interval = setInterval( () => {
 
         // height of parent and element
@@ -38,7 +64,6 @@ const NgTrackUser = () => {
           fieldHeight = nativeEl.getBoundingClientRect().height,
 
           // dimensions of parent and element  
-          // TODO: use this property and test
           viewPortDimensions = {
             top: scrollableParent.scrollTop,
             bottom: scrollableParent.scrollTop + viewPortHeight,
@@ -51,38 +76,55 @@ const NgTrackUser = () => {
             name: nativeEl.id
           };
 
-        // on tab change
         visibly.onHidden(function() {
           isPaused = true;
         });
     
-        // on current tab
         visibly.onVisible(function() {
           isPaused = false;
-        });      
+        });
 
         // check if element in view
-        if (isElementInView(fieldDimensions, viewPortDimensions) && !isPaused) {          
-          // incerment the time
-          timer++;          
-          // log the time for section
-          console.log(nativeEl.id, timer);
+        if (isElementInView(fieldDimensions, viewPortDimensions) && !isPaused && window.timer) {
+          
+          // add the event with start time to window.timer if not present
+          if(!window.timer[nativeEl.id]) {
+            window.timer = {
+              ...window.timer,
+              [nativeEl.id]: 0
+            };
+          }
+          
+          window.timer[nativeEl.id]++;
+          
+          // TODO: remove this console log before use          
+          console.log(nativeEl.id, window.timer[nativeEl.id]);
         }
-      }, 1000
-    );
 
-    // on destroy of directive
-    element.on('$destroy', () => {
+        if(window.globalWatcher && 
+           window.globalWatcher === globalWatcherLimit && 
+           window.timer && 
+           window.trackingCalls && 
+           window.resetEventsTime) {
+          
+          // TODO: remove this console log before use
+          console.log(window.timer);
+          window.trackingCalls();
+          window.resetEventsTime();
+          window.globalWatcher = 0;
+        }
 
-      // clear the interval to prevent memory leak
-      clearInterval(interval);
-      if(timer) {
-        // log the final time for the element
-        console.log(nativeEl.id, timer);
-        /* Call your trackings here.*/
-      }
-    });
-    
+      }, 1000);
+
+      element.on('$destroy', () => {
+        clearInterval(interval);
+        if(window.timer[nativeEl.id] || window.timer[nativeEl.id] === 0) {
+          delete window.timer[nativeEl.id];
+        }
+        if(window.globalWatcher) {
+          delete window.globalWatcher;
+        }
+      });
     }
   }
 };
